b5871ee7f6ece686b322fd912002e20ba2087a83
Iago Neres <iagoneresb@gmail.com>
2018-05-30 01:20:50 -0300

    Pages | Ajustar página de perfil

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,25 +1,53 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the ProfilePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { Component }        from '@angular/core';
+import { Camera }           from '@ionic-native/camera';
+import { IonicPage, NavController, NavParams, ToastController, AlertController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
   selector: 'page-profile',
   templateUrl: 'profile.html',
 })
-export class ProfilePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+export class ProfilePage {
+  errorMessage: any;
+  placeholderPicture = 'https://api.adorable.io/avatar/200/bob';
+
+  enableNotifications = true;
+  language: any;
+  currency: any;
+  paymentMethod: any;
+
+  languages = ['English', 'Portuguese', 'French'];
+  paymentMethods = ['Paypal', 'Credit Card'];
+  currencies = ['USD', 'BRL', 'EUR'];
+
+  user = {
+      name: 'Marty Mcfly',
+      imageUrl: 'assets/imgs/marty-avatar.png'
+  };
+
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public toastCtrl: ToastController,
+              public alertCtrl: AlertController) {
+  }
+
+  toggleNotifications() {
+      if (this.enableNotifications) {
+          this.toastCtrl.create({
+              message: 'Notifications enabled',
+              duration: 3000
+          });
+      } else {
+              this.toastCtrl.create({
+                  message: 'Notifications enabled',
+                  duration: 3000
+              });
+      }
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ProfilePage');
+
   }
 
 }
